2e820a9f4cd8560e43261dd341688a75e852aa99
RicardoMoo <RicardoMoo@LAPTOP-QLOP7A9S>
2021-02-18 20:20:43 -0600

    //18/2/2021// igualando las dos copias, el working copy y el local repositori

---
+++ src/practicas_individduales/PRACTICAS_INDIVIDDUALES.java
@@ -0,0 +1,30 @@
+
+package practicas_individduales;
+
+import java.util.Scanner;
+
+public class PRACTICAS_INDIVIDDUALES {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Bienvenido, es hora de Programas uwu");
+        int matricula=66208;
+        String wafle = new String(); 
+        Scanner read = new Scanner (System.in);
+        String dataRead[][] = new String[3][0]; 
+        int reply = 0 ;
+        System.out.println("¿Cuanto es 4x5?");
+        reply = read.nextInt();
+        
+        if (reply == 20){
+            System.out.println("Correcto");
+        }
+        else
+        {
+            System.out.println("Incorrecto");
+        }    
+        System.out.println("Soy Ricardo Moo Vargas con la matricula:"+ matricula);
+        //owo
+    }
+    
+}
